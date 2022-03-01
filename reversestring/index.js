e807a0696863d6a3e42759a5239f763e78690617
Sandi Ibraimoff <sam.ibraimoff@gmail.com>
2022-03-01 03:54:44 -0500

    reverse string exercises

--- reversestring/index.js
+++ reversestring/index.js
@@ -6,8 +6,35 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
+// solution #1 using reverse method
+// function reverse(str) {
+//   return str.split('').reverse().join('');
+// }
+
+// solution #2 using loop
+// function reverse(str) {
+//   let reversed = [];
+//   let strArr = str.split('');
+//   for (let i = 0; i < strArr.length; i++) {
+//     reversed.unshift(strArr[i]);
+//   }
+//   return reversed.join('');
+// }
+
+// solution #3 using loop without converting str into array
+// function reverse(arr) {
+//   let reversed = '';
+
+//   for (let char of arr) {
+//     reversed = char + reversed;
+//   }
+
+//   return reversed;
+// }
+
+// solution #4 using reduce
 function reverse(str) {
-  console.log('reverse string');
+  return str.split('').reduce((rev, char) => char + rev, '');
 }
 
 module.exports = reverse;
